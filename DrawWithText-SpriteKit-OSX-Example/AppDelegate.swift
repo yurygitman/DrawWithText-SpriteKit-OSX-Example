22fcc3d194f40016e6e26a86d66d2a982369b961
samuraipapa <yury.gitman@gmail.com>
2015-05-07 11:42:08 -0400

    Initial Commit

---
+++ DrawWithText-SpriteKit-OSX-Example/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  DrawWithText-SpriteKit-OSX-Example
+//
+//  Created by GrownYoda on 5/7/15.
+//  Copyright (c) 2015 yuryg. All rights reserved.
+//
+
+
+import Cocoa
+import SpriteKit
+
+extension SKNode {
+    class func unarchiveFromFile(file : String) -> SKNode? {
+        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
+            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+}
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var skView: SKView!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        /* Pick a size for the scene */
+        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            self.skView!.presentScene(scene)
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            self.skView!.ignoresSiblingOrder = true
+            
+            self.skView!.showsFPS = true
+            self.skView!.showsNodeCount = true
+        }
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
+}
---
+++ DrawWithText-SpriteKit-OSX-ExampleTests/DrawWithText_SpriteKit_OSX_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  DrawWithText_SpriteKit_OSX_ExampleTests.swift
+//  DrawWithText-SpriteKit-OSX-ExampleTests
+//
+//  Created by GrownYoda on 5/7/15.
+//  Copyright (c) 2015 yuryg. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class DrawWithText_SpriteKit_OSX_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
